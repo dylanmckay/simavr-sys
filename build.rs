65f1a72f8b17cc49a3290a45a40dcc8276d46de8
Dylan McKay <me@dylanmckay.io>
2017-07-07 00:47:08 +1200

    Check the bindings into version control
    Until a bug is fixed, we cannot parse avrsim headers, so I've checked
    the version generated by the patched bindgen into version control.
    
    This should be reverted once server/rust-bindgen#426 is fixed.

--- build.rs
+++ build.rs
@@ -2,16 +2,16 @@ extern crate bindgen;
 
 use bindgen::builder;
 
-const BINDINGS_DEST: &'static str = "src/bindings.rs";
+// const BINDINGS_DEST: &'static str = "src/bindings.rs";
 
 fn main() {
     // Configure and generate bindings.
-    let bindings = builder().header("simavr/simavr/sim/sim_avr.h")
+    let _bindings = builder().header("simavr/simavr/sim/sim_avr.h")
                             .generate()
                             .expect("could not generate bindings");
 
     // Write the generated bindings to an output file.
-    bindings.write_to_file(BINDINGS_DEST)
-        .expect("could not write bindings to file");
+    // bindings.write_to_file(BINDINGS_DEST)
+    //     .expect("could not write bindings to file");
 }
 
